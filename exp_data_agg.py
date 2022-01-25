42f716752c36be4f3045b5e214df20a62f95798a
PythonNut <PythonNut@users.noreply.github.com>
2022-01-25 00:46:38 -0800

    Add script to aggregate experimental results into DataFrame

---
+++ exp_data_agg.py
@@ -0,0 +1,27 @@
+import pickle
+import argparse
+from pathlib import Path
+import pandas as pd
+import itertools as it
+
+parser = argparse.ArgumentParser()
+parser.add_argument("dir", type=Path)
+args = parser.parse_args()
+root = args.dir
+
+assert root.exists()
+
+algs = ["decomp_2d", "decomp_mip", "optprox", "explp", "anderson", "lp"]
+props = ["bound", "time"]
+df = pd.DataFrame(columns=list(it.product(algs, props)))
+
+for f in sorted(root.glob("[0-9]*.pkl"), key=lambda f: int(f.stem)):
+    i = int(f.stem)
+    data = pickle.load(open(f, "rb"))
+    df.loc[i] = {
+        (alg, col): t[j] for alg, t in data.items() for j, col in enumerate(props)
+    }
+
+print(df)
+
+df.to_pickle(root / f"{root.stem}.pkl")
