832b9f3f9999a89778b20ef076fc0f49d5e1f7b3
PythonNut <PythonNut@users.noreply.github.com>
2022-01-25 23:04:03 -0800

    Make experimental data aggregator more robust

--- exp_data_agg.py
+++ exp_data_agg.py
@@ -3,6 +3,7 @@ import argparse
 from pathlib import Path
 import pandas as pd
 import itertools as it
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("dir", type=Path)
@@ -11,15 +12,23 @@ root = args.dir
 
 assert root.exists()
 
-algs = ["decomp_2d", "decomp_mip", "optprox", "explp", "anderson", "lp"]
+algs = set()
 props = ["bound", "time"]
+data = {}
+
+for f in root.glob("[0-9]*.pkl"):
+    i = int(re.match("[0-9]+", f.stem).group())
+    run_data = pickle.load(open(f, "rb"))
+    data.setdefault(i, {}).update(run_data)
+    algs.update(run_data.keys())
+
+print(sorted(algs))
+
 df = pd.DataFrame(columns=list(it.product(algs, props)))
 
-for f in sorted(root.glob("[0-9]*.pkl"), key=lambda f: int(f.stem)):
-    i = int(f.stem)
-    data = pickle.load(open(f, "rb"))
+for i in sorted(data.keys()):
     df.loc[i] = {
-        (alg, col): t[j] for alg, t in data.items() for j, col in enumerate(props)
+        (alg, col): t[j] for alg, t in data[i].items() for j, col in enumerate(props)
     }
 
 print(df)
